05a39f35602803340301ce9952c3732766eb7178
EmmanuelFenyi <kwamefemma@gmail.com>
2021-03-13 00:40:01 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./bootstrap.css";
+import Navigation from "./Components/Navigation/Navigation";
+import Header from "./Components/Header/Header";
+import AboutSection from "./Components/AboutSection/AboutSection";
+import InventorySection from "./Components/InventorySection/InventorySection";
+import ContactSection from "./Components/ContactSection/ContactSection";
+import Footer from "./Components/Footer/Footer";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Navigation />
+
+    <Header />
+
+    <main>
+      <AboutSection />
+
+      <div id="hr">
+        <hr className="container" />
+      </div>
+
+      <InventorySection />
+
+      <ContactSection />
+    </main>
+
+    <Footer />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
